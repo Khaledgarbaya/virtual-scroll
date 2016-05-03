71c486c047d21e1dcf387b2ee896d2b06188463f
Khaledgarbaya <khaledgarbaya@gmail.com>
2016-05-03 12:33:20 +0200

    Added VirtualScroll API
    - Added required methods/properties
    - Added Unit test for the required api

--- src/VirtualScroll.js
+++ src/VirtualScroll.js
@@ -1,5 +1,60 @@
 export default class VirtualScroll {
-  constructor() {
+  /**
+   * constructor - VirtualScroll
+   *
+   * @param  {object} config
+   * {
+   *  // DOMnode the list will be renderd inside
+   *  root: document.getElementsByClassName('NAME_THE_CONTAINER_WHATEVER_YOU_LIKE')[0],
+   *  // ARRAY containg all the objects to be displayed inside the list
+   *  source: listSource,
+   *  //FUNCTION to be called while creating a single item
+   *    itemNodes empty object provided by the list logic
+   *    itemContainer   item container provided by the list logic
+   *  createItemFn: (itemNodes, itemContainer) =>{},
+   *
+   * //FUNCTION to be called while updating a single item
+   *   //itemNodes       object previously filled by createItemFn()
+   *  //itemContainer   item container provided by the list logic
+   *  //itemData        object inside listSource array at the current index
+   * updateItemFn: (itemNodes, itemContainer, itemData) => {},
+   * }
+   */
+  constructor (config) {
+    this.config = {};
+    this.info= {scrollTop:0, direction:1, height:1, isScrolling:false};
+    this.config.root = (config && config.root) ? config.root : null;
+    this.config.source = (config && config.root) ? config.source : [];
+    this.config.createItemFn = (config && config.root) ? config.createItemFn : null;
+    this.config.updateItemFn = (config && config.root) ? config.updateItemFn : null;
+  }
+  /*Public API*/
+  // TODO add documentation plz!
+  refresh (){
+
+  }
+  setSource (listSource){
+
+  }
+  destroy (){
+
+  }
+  remove (){
+
+  }
+  addEventListener (event, callback){
+
+  }
+  removeEventListener (event, callback){
+
+  }
+  scrollTop (duration){
+
+  }
+  scrollBottom (duration){
+
+  }
+  scrollTo (position, duration){
 
   }
 }
---
+++ test/virtual-list.spec.js
@@ -0,0 +1,77 @@
+import {expect} from 'chai';
+import VirtualScroll from '../src/VirtualScroll.js';
+/*
+  Mama always says three things:
+  - Finish your plate
+  - Drink your milk
+  - Test your code.
+*/
+describe('VirtualScroll', () => {
+  const vScroll = new VirtualScroll();
+  describe('initial state', ()=>{
+    it('vScroll is instannce of VirtualScroll', ()=>{
+      expect(vScroll).to.be.an.instanceof(VirtualScroll);
+    });
+    it('should have a property config', ()=>{
+      expect(vScroll).to.have.property('config');
+    });
+  });
+  describe('VirtualScroll::config', ()=>{
+    it('should have root, source, createItemFn and updateItemFn', ()=>{
+      expect(vScroll.config).to.have.property('root');
+      expect(vScroll.config).to.have.property('source');
+      expect(vScroll.config).to.have.property('createItemFn');
+      expect(vScroll.config).to.have.property('updateItemFn');
+    });
+  });
+  describe('VirtualScroll required API methods',()=>{
+    it('should have refresh function', ()=>{
+      expect(vScroll).to.have.property('refresh').that.is.a('function');
+    });
+    it('should have setSource function', ()=>{
+      expect(vScroll).to.have.property('setSource').that.is.a('function');
+    });
+    it('should have destroy function', ()=>{
+      expect(vScroll).to.have.property('destroy').that.is.a('function');
+    });
+    it('should have remove function', ()=>{
+      expect(vScroll).to.have.property('remove').that.is.a('function');
+    });
+    /*Optional API*/
+    it('should have addEventListener function', ()=>{
+      expect(vScroll).to.have.property('addEventListener').that.is.a('function');
+    });
+    it('should have removeEventListener function', ()=>{
+      expect(vScroll).to.have.property('removeEventListener').that.is.a('function');
+    });
+    it('should have scrollTop function', ()=>{
+      expect(vScroll).to.have.property('scrollTop').that.is.a('function');
+    });
+    it('should have scrollBottom function', ()=>{
+      expect(vScroll).to.have.property('scrollBottom').that.is.a('function');
+    });
+    it('should have scrollTo function', ()=>{
+      expect(vScroll).to.have.property('scrollTo').that.is.a('function');
+    });
+  });
+
+  describe('VirtualScroll required API properties',()=>{
+    it('should have info property', ()=>{
+      expect(vScroll).to.have.property('info').that.is.a('object');
+    });
+    describe('VirtualScroll::info property', ()=>{
+      it('should have a numeric property called scrollTop',()=>{
+        expect(vScroll.info).to.have.property('scrollTop').that.is.a('number');
+      });
+      it('should have a numeric property called direction',()=>{
+        expect(vScroll.info).to.have.property('direction').that.is.a('number');
+      });
+      it('should have a numeric property called height',()=>{
+        expect(vScroll.info).to.have.property('height').that.is.a('number');
+      });
+      it('should have a boolean property called isScrolling',()=>{
+        expect(vScroll.info).to.have.property('isScrolling').that.is.a('boolean');
+      });
+    });
+  });
+});
