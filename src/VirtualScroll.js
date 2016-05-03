faad2f68b1352d65cbfb1d8ed8ff6cac0c3931ed
Khaledgarbaya <khaledgarbaya@gmail.com>
2016-05-04 00:03:25 +0200

    Separate the scrolling and User Action in one class
    All the user interaction and scroll calculation are done separately in
    VerticalScroller, you can add easily a HorizontalScroller

--- src/VirtualScroll.js
+++ src/VirtualScroll.js
@@ -1,3 +1,4 @@
+import VirtualScroller from './VerticalScroller.js'
 export default class VirtualScroll {
   /**
    * constructor - VirtualScroll
@@ -23,36 +24,37 @@ export default class VirtualScroll {
   constructor (config) {
     this.config = {};
     this.info= {scrollTop:0, direction:1, height:1, isScrolling:false};
-    this.config.root = (config && config.root) ? config.root : null;
+    this.config.root = (config && config.root) ? config.root : {};
     this.config.source = (config && config.root) ? config.source : [];
     this.config.createItemFn = (config && config.root) ? config.createItemFn : null;
     this.config.updateItemFn = (config && config.root) ? config.updateItemFn : null;
-
+    this.rootElement = null;
+    // setup the container
+    this._setupContainer();
+    this.config.scroller = (config && config.scroller) ? config.scroller : new VirtualScroller(this.config.root, (position)=>{
+      this._scroll(position);
+    });
     this.totalRows = this.config.source.length;
     // TODO try to get dynamic itemHeight
     this.itemHeight = 50;
     this.visibleItemsCount = Math.ceil(config.root.offsetHeight / this.itemHeight);
     this.cachedItemsLen = this.visibleItemsCount * 3;
-    // setup the container
-    this._setupContainer();
-    this._renderChunk(this.config.root, 0)
-    const scroller = this._createScroller(this.totalRows*this.itemHeight);
-    this.config.root.appendChild(scroller);
+    this._renderChunk(this.rootElement, 0)
+    this.scrollerElement = this._createScroller(this.totalRows*this.itemHeight);
+    this.rootElement.appendChild(this.scrollerElement);
     this.lastRepaintY = 0;
     this.maxBuffer = this.visibleItemsCount * this.itemHeight;
     this.lastScrolled = 0;
-
     this.rmNodeInterval = setInterval(this.remoDirtyNodes.bind(this), 300);
 
     this._bindEvents();
     this.onScroll = this.onScroll.bind(this);
   }
   remoDirtyNodes(){
-    console.log(this.lastScrolled);
     if (Date.now() - this.lastScrolled > 100) {
       let badNodes = document.querySelectorAll('[data-rm="1"]');
       for (let i = 0, l = badNodes.length; i < l; i++) {
-        this.config.root.removeChild(badNodes[i]);
+        this.rootElement.removeChild(badNodes[i]);
       }
     }
   }
@@ -61,20 +63,21 @@ export default class VirtualScroll {
   }
   onScroll (e){
     let scrollTop = e.target.scrollTop; // Triggers reflow
-    if (!this.lastRepaintY || Math.abs(scrollTop - this.lastRepaintY) > this.maxBuffer) {
-      let first = parseInt(scrollTop / this.itemHeight) - this.visibleItemsCount;
-      console.log(first);
-      this._renderChunk(this.config.root, first < 0 ? 0 : first);
-      this.lastRepaintY = scrollTop;
-    }
-    this.lastScrolled = Date.now();
+    this._scroll(scrollTop);
     e.preventDefault && e.preventDefault();
   }
   _setupContainer (){
-    this.config.root.style.overflow = 'auto';
+    this.rootElement = document.createElement('div');
+    this.rootElement.style.width = this.config.root.clientWidth+'px';
+    this.rootElement.style.height = this.config.root.clientHeight+'px';
+    //this.rootElement.style.overflow = 'hidden';
+
+    this.config.root.style.overflow = 'hidden';
     this.config.root.style.position = 'relative';
     this.config.root.style.padding = 0;
     this.config.root.style.border = '1px solid black';
+
+    this.config.root.appendChild(this.rootElement);
   }
   _createScroller (h){
     const scroller = document.createElement('div');
@@ -87,7 +90,7 @@ export default class VirtualScroll {
     return scroller;
   }
   _renderChunk (node, from) {
-    var finalItem = from + this.cachedItemsLen;
+    let finalItem = from + this.cachedItemsLen;
     if (finalItem > this.totalRows)
       finalItem = this.totalRows;
 
@@ -100,8 +103,10 @@ export default class VirtualScroll {
 
     // Hide and mark obsolete nodes for deletion.
     for (var j = 1, l = node.childNodes.length; j < l; j++) {
-      node.childNodes[j].style.display = 'none';
-      node.childNodes[j].setAttribute('data-rm', '1');
+      if(this.scrollerElement != node.childNodes[j]){
+        node.childNodes[j].style.display = 'none';
+        node.childNodes[j].setAttribute('data-rm', '1');
+      }
     }
     node.appendChild(fragment);
   }
@@ -115,13 +120,34 @@ export default class VirtualScroll {
       nodeItem.style.top = (index * this.itemHeight) + 'px';
     }
   }
+  _scroll(position){
+    //Update top of the root element
+    if(position<0){
+      position = 0;
+    }
+    var t = 'translateY(' + (-position) + 'px) translateZ(0)';
+    var s = this.rootElement.style;
+    s["transform"] = t;
+    s["webkitTransform"] = t;
+    s["mozTransform"] = t;
+    s["msTransform"] = t;
+
+    if (!this.lastRepaintY || Math.abs(position - this.lastRepaintY) > this.maxBuffer) {
+      let first = parseInt(position / this.itemHeight) - this.visibleItemsCount;
+      this._renderChunk(this.rootElement, first < 0 ? 0 : first);
+      this.lastRepaintY = position;
+    }
+    this.lastScrolled = Date.now();
+  }
   /*Public API*/
+
   // TODO add documentation plz!
   refresh (){
 
   }
   setSource (listSource){
-
+    this.config.source = listSource;
+    refresh();
   }
   destroy (){
 
@@ -136,12 +162,14 @@ export default class VirtualScroll {
 
   }
   scrollTop (duration){
-
+    // TODO addAnimation
+    this._scroll(0);
   }
   scrollBottom (duration){
-
+    this._scroll(this.totalRows);
   }
   scrollTo (position, duration){
-
+    // TODO add Animation with duration
+    this._scroll(position);
   }
 }
---
+++ test/virtual-scroller.spec.js
@@ -0,0 +1,11 @@
+import {expect} from 'chai';
+import VirtualScroller from '../src/VirtualScroller.js';
+describe('VirtualScroller', () => {
+    const virtualScroller = new VirtualScroller({},()=>{});
+    it('should have a property called scrollContainer', ()=>{
+      expect(virtualScroller).to.have.property('scrollContainer');
+    });
+    it('should have a property called scrollCallback', ()=>{
+      expect(virtualScroller).to.have.property('scrollCallback');
+    });
+});
